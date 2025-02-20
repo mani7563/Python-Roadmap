3e2ceeb4b0eb64a8bdcbb6be3ccb59b08333a832
mani7563 <manikantamanthri1617@gmail.com>
2025-02-20 06:57:22 +0530

    Python programming roadmap

---
+++ Python_roadmap.py
@@ -0,0 +1,13 @@
+""" Python Programming """
+
+""" Topics"""
+"""1.Getting Started with Python Programming"""
+"""1.Introduction to Python"""
+"""2.Compiler vs Interpreter Language"""
+"""3.Setting up Python work environment"""
+"""4.Hello World in Python (running Python codes in 3 ways)"""
+"""5.Print Statement"""
+"""6.Variables"""
+"""7.Comments"""
+"""8.Data Types"""
+"""9.Getting the input from the user."""

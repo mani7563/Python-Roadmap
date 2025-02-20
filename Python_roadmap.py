2104f0160916716d197b1bf4d197618c203fd514
mani7563 <manikantamanthri1617@gmail.com>
2025-02-20 08:47:02 +0530

    Python programming roadmap

--- Python_roadmap.py
+++ Python_roadmap.py
@@ -1,7 +1,9 @@
 """ Python Programming """
 
 """ Topics"""
-"""1.Getting Started with Python Programming"""
+
+"""1.Getting Started with Python"""
+
 """1.Introduction to Python"""
 """2.Compiler vs Interpreter Language"""
 """3.Setting up Python work environment"""
@@ -11,3 +13,121 @@
 """7.Comments"""
 """8.Data Types"""
 """9.Getting the input from the user."""
+
+
+"""2.Operators in Python"""
+
+"""1.Arithmetic Operators"""
+"""2.Comparision Operators"""
+"""3.Assignment Operators"""
+"""4.Logical Operators"""
+"""5.Bitwise Operators"""
+"""6.Membership Operators"""
+"""7.Operator Precedence"""
+"""8.String Operations"""
+"""9.Type Casting-Implicit & Explicit Conversions"""
+"""10.Unicode Representaitions"""
+
+
+"""3.Conditional Statements in Python"""
+
+"""1.if else statements"""
+"""2.elif statements"""
+"""3.Nested if else statements"""
+"""4.Ternary Statements"""
+
+
+"""4.Loops in Python"""
+
+"""1.for loop"""
+"""2.while loop"""
+"""3.break statement"""
+"""4.continue statement"""
+"""5.pass statement"""
+"""6.Nested Loops"""
+
+
+"""5.Lists in Python"""
+
+"""1.Initializing a list"""
+"""2.Access a list"""
+"""3.Indexing in a list"""
+"""4.length of list"""
+"""5.User input of a list"""
+"""6.Adding elements to a list"""
+"""7.Removing elements from a list"""
+"""8.Slicing a list"""
+"""9.Built-in-methods for Python Lists"""
+"""10.Reversing a list"""
+
+
+"""6.Tuple in Python"""
+
+"""1.Initializing a tuple"""
+"""2.Access a tuple"""
+"""3.Indexing in a tuple"""
+"""4.length of a tuple"""
+"""5.Mutability"""
+"""6.How to add elements/remove elements using a tuple"""
+"""7.Built-in-methods for Python Lists"""
+
+
+"""7.Set in Python"""
+
+
+"""1.Initializing a Set"""
+"""2.Access a set"""
+"""3.length of a set"""
+"""4.Mutability for set"""
+"""5How to add elements/remove in a set"""
+"""6.Frozen sets"""
+"""7.Built-in-methods for Python sets"""
+
+"""8.Dictionary in Python"""
+
+
+"""1.Initializing a Dictionary"""
+"""2.Access a Dictionary"""
+"""Length of a Dictionary"""
+"""Mutability for dictionary"""
+"""Add/remove items in a dictionary"""
+"""Copy a dictionary"""
+"""Nested dictionaries"""
+"""Built-in-methods for Python dictionary"""
+
+
+"""9.Strings in Python"""
+
+
+"""1.What is a string"""
+"""2.Indexing in a string"""
+"""3.String Reversal"""
+"""4.Escape Sequencing"""
+"""5.Updating/Deleting an element in a string"""
+"""6.Formatting a string"""
+"""7.String operations"""
+"""8.Slicing a string"""
+"""9.Built-in-methods for Python strings"""
+
+
+"""10.Functions in Python"""
+
+
+"""1.what is a function"""
+"""2.Function declaration"""
+"""3.Built-in-Functions"""
+"""4.User-defined functions"""
+"""5.Docstring in functions"""
+"""6.Functiona Arguments"""
+"""6.1.Default Arguments"""
+"""6.2.Keyword Arguments"""
+"""6.3.Positional Arguments"""
+"""6.4.Arbitrary Arguments"""
+"""7.Nested  Functions"""
+"""8.Anonymous Functions"""
+"""9.Return statement"""
+"""10.Pass by Reference & Pass by Value"""
+"""11.Recursive Functions"""
+
+
+
